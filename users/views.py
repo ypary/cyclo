a4731937e0861881ad7ef586e537286024497f2a
Alice <sergeymrk@gmail.com>
2025-01-26 16:10:38 +0300

    Создана регистрация пользователя (добавление пользователя в базу данных); Создана логика работы с авторизацией и регистрацией;

--- users/views.py
+++ users/views.py
@@ -1,9 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from users.models import User
+from users.forms import UserLoginForm, UserRegisterForm
+from django.contrib import auth
+from django.urls import reverse
 
 # Create your views here.
 
 def login(request):
-    return render(request, 'users/login.html')
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+    context = {'form': form}
+    return render(request, 'users/login.html', context)
 
 def registration(request):
-    return render(request, 'users/registration.html')
+    if request.method == 'POST':
+        form = UserRegisterForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:login'))
+    else:
+        form = UserRegisterForm()
+    context = {'form': form}
+    return render(request, 'users/registration.html', context)
